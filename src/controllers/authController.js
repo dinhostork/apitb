04888387583de5fad31fd4f9db1d8a885e41341c
Dinho Stork <dinhostork@live.com>
2019-11-08 08:26:20 -0300

    novo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
  
-router.get('/ola', async (req, res) => {
-    return res.status(200).send({ok: true})
-} )
+router.post('/login', (req, res) => {
+    const { email, password } = req.body
+    
+})
+
+router.post('/register', (req, res) => {
+    const { username, email, password } = req.body 
+    //verificações de existencia   
+})
+
+router.post('/forget', (req, res) => {
+    const { email } = req.body
+    //verificações de existencia
+})
+
+router.post('/reset', (req, res) => {
+    //verificar se o código confere
+})
+
+
 
 module.exports = app => app.use('/auth', router)
